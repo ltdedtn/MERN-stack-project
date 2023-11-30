9133e5b2e220d95da47c368bece017eb301ba112
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2023-11-30 19:10:40 +1300

    adding cors

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
