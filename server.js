de149da3a6aeb472d908adbaf2f426d36d92a841
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2023-11-30 19:58:24 +1300

    finshed connecting DB

--- server.js
+++ server.js
@@ -49,4 +49,8 @@ mongoose.connection.once("open", () => {
 
 mongoose.connection.on("error", (err) => {
   console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrors.log"
+  );
 });
