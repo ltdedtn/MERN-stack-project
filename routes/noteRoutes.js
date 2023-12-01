f18dfcb8e72d200261ff725c31d5a06f95ca5ae0
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2023-12-01 18:50:38 +1300

    starting to add notes

---
+++ routes/noteRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const noteControllers = require("../controllers/noteControllers");
+
+router
+  .route("/")
+  .get(noteControllers.getAllNotes)
+  .post(noteControllers.createNewNote)
+  .patch(noteControllers.updateNote)
+  .delete(noteControllers.deleteNote);
+
+module.exports = router;
