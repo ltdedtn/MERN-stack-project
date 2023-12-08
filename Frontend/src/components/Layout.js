16a7ef086f3dff0142981836d4b9c752d760230c
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2023-12-08 17:53:01 +1300

    dark/light patch

--- Frontend/src/components/Layout.js
+++ Frontend/src/components/Layout.js
@@ -3,11 +3,7 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 
 const Layout = () => {
-  return (
-    <div className="w-screen h-screen" data-theme="dark">
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 };
 
 export default Layout;
