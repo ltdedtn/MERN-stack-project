c264b7fb66417388b08670a4cca74427d507db8d
ltdedtn <34638758+ltdedtn@users.noreply.github.com>
2023-12-13 16:37:42 +1300

    made role selection for new user nicer

--- Frontend/src/features/auth/Welcome.js
+++ Frontend/src/features/auth/Welcome.js
@@ -2,15 +2,21 @@ import { Link } from "react-router-dom";
 
 const Welcome = () => {
   const content = (
-    <section className="welcome">
-      <h1>Welcome to the Dashboard</h1>
+    <>
+      <b>Welcome to the dashboard</b>
+      <p>
+        <Link to="/dash/users">View Users</Link>
+      </p>
+      <p>
+        <Link to="/dash/users/new">Create a new User</Link>
+      </p>
       <p>
         <Link to="/dash/notes">View techNotes</Link>
       </p>
       <p>
-        <Link to="/dash/users">View Users</Link>
+        <Link to="/dash/notes/new">Create a new techNote</Link>
       </p>
-    </section>
+    </>
   );
 
   return content;
